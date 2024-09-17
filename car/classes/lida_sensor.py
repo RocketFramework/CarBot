9553037f694d24123df2b8ad8662b73423dcbefc
Your Name <you@example.com>
2024-09-17 15:51:45 +0530

    Lidar sensor Error Fixed

--- car/classes/lida_sensor.py
+++ car/classes/lida_sensor.py
@@ -3,23 +3,26 @@ import serial, time
 import numpy
 
 # Define the UART Device
-Lidar_Sensor_Port = serial.Serial("/dev/serial0", 115200,timeout=0)
+Lidar_Sensor_Port = serial.Serial("/dev/serial0", 115200,timeout=1)
 
 # Read the distance to the obstacle
 def Get_Distance_To_Obstacle():
-    Count = Lidar_Sensor_Port.in_waiting
-    if Count > 8:
-        Bytes = ser.read(9)
-        Lidar_Sensor_Port.reset_input_buffer()
-        
-        if bytes_serial[0] == 0x59 and bytes_serial[1] == 0x59:
-            distance = Bytes[2] + Bytes[3]*256
-            return distance/100.0
-        
+    while True:
+        Count = Lidar_Sensor_Port.in_waiting
+        if Count > 8:
+            Bytes = Lidar_Sensor_Port.read(9)
+            Lidar_Sensor_Port.reset_input_buffer()
+        #   print(Bytes)
+            if Bytes[0] == 0x59 and Bytes[1] == 0x59:
+                distance = Bytes[2] + Bytes[3]*256
+                return distance/100
+                time.sleep(0.1)
+            
 if not Lidar_Sensor_Port.isOpen():
     Lidar_Sensor_Port.open()
 
-distance = Get_Distance_To_Obstacle()
-print(distance)
+while True:
+    distance = Get_Distance_To_Obstacle()
+    print(f"Distance To Obstacle : {distance} m")
 
 Lidar_Sensor_Port.close()
