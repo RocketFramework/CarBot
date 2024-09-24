2fd73164debd0d7fac708809a4d0af745ea0616b
rocketframework <c_nirosh@yahoo.com>
2024-09-24 21:35:43 +0530

    added some new code for autopilot, but still the import issue is there, as you can see in AutoPilot LidaSensor and CarEngine cannot access their methods

--- car/auto_pilot.py
+++ car/auto_pilot.py
@@ -1,4 +1,6 @@
-from car_config import lida_sensor
+from car_config import lida_sensor, MINIMUM_GAP, car_engine, WHEEL_RADIUS, ONE_WHEEL_TURN_LENGTH, ONE_WHEEL_TURN_STEPS
+import math
+import time
 
 class Auto_Pilot:
     def __int__(self):
@@ -7,12 +9,27 @@ class Auto_Pilot:
     def find_direction(self):
         # Get the input from lida
         self.lidarSensor = LidarSensor()
+        self.carEngine = CarEngine()
         distance = self.lidarSensor.get_distance_to_obstacle()
         # if a distance is long enough
-    
-           
-        
-        # Move the car
+        if distance > MINIMUM_GAP:
+            steps = int(((distance - MINIMUM_GAP)/ONE_WHEEL_TURN_LENGTH) * ONE_WHEEL_TURN_STEPS)
+            # Move the car forward
+            self.carEngine.move_forward(steps)
+        else:
+            #Move head to left
+            #TBD
+            distance_l = self.lidarSensor.get_distance_to_obstacle()
+            #wait   
+            time.sleep(1)
+            #Move head to right
+            #TBD
+            distance_r = self.lidarSensor.get_distance_to_obstacle()
+            #find the direction that has move space
+            if (distance_l > distance_r):
+                distance = distance_l
+            else:
+                distance = distance_r
         # else
         # trun the car and get the input from Lida
     
--- car/car_config.py
+++ car/car_config.py
@@ -1,12 +1,17 @@
 import sys
 import platform
 from pathlib import Path
-
+import math
 # Add the full path of 'my_folder' to sys.path
 if platform.system() == "Windows":
     sys.path.append(str(Path(r"C:\Users\user_\CarBot\car\classes")))
 else:
     sys.path.append(str(Path("/home/pi/Desktop/CarBot/car/classes")))
+
+MINIMUM_GAP = 0.33
+WHEEL_RADIUS = 0.002
+ONE_WHEEL_TURN_LENGTH = 2*math.pi*WHEEL_RADIUS
+ONE_WHEEL_TURN_STEPS = 26
 
 import lida_sensor
 import mock_serial
