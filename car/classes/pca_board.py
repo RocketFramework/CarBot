04247e2e0bef665d5acc43db30d30d566a11f40c
rocketframework <c_nirosh@yahoo.com>
2024-09-30 20:34:23 +0530

    bug fixed

--- car/classes/pca_board.py
+++ car/classes/pca_board.py
@@ -39,8 +39,8 @@ class PCABoard():
             if (pcaServo.servo_id == ServoIds.Driver):
                 self._driver_servo = self.PcaServos
                 
-            self.kit.servo[self.PcaServo.servo_id].actuation_range = pcaServo.actuation_range        
-            pcaServo.rotate(self.PcaServo.servo_id, pcaServo.angle)
+            self.kit.servo[pcaServo.servo_id].actuation_range = pcaServo.actuation_range        
+            pcaServo.rotate(pcaServo.servo_id, pcaServo.angle)
             time.sleep(.1)  
    
     @property
