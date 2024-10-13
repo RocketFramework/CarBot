58c1b66052bdb4024ff1d9ddde4f4aef770e6b65
Basilu Liyanage <basilu.research@gmail.com>
2024-10-13 09:01:21 +0530

    Updated Codes 9:01am

--- build.py
+++ build.py
@@ -6,7 +6,7 @@ import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 # Define the modules you want to import and use
-modules = ['car.classes.car_engine']
+modules = ['car.full_self_driving']
 
 # Iterate through each module
 for module_name in modules:
