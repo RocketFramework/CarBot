fe04d90f6b3d925ed3a4b1a2f2a07157716ea4ba
Basilu Liyanage <basilu.research@gmail.com>
2024-10-22 17:45:59 +0530

    Finalized system - Car ready for Test Drive ( Version 2 )

---
+++ run_server.py
@@ -0,0 +1,21 @@
+import importlib
+import sys
+import os
+# Add the root path to sys.path so packages can be found
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+# from car import full_self_driving
+
+# full_self_driving.run()
+# # Define the modules you want to import and use
+modules = ['telemetry.server']
+# #modules = ['car.classes.lida_sensor']
+# # Iterate through each module
+for module_name in modules:
+    # Import the module dynamically
+    module = importlib.import_module(module_name)
+
+    # Optionally call a function or class from each module
+    # Assuming each module has a 'run' function for example
+    if hasattr(module, 'start_server'):
+        module.start_server()
